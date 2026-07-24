74dcc2ba5fdd7dd2cec74986d228b3e632745590
ShamimSB <bhuiyanshamim09@gmail.com>
2026-07-24 09:12:25 +0600

    Fix routing and component folder path

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,29 @@
 // import React from 'react';
-import Navbar from './Conponent/Navbar';
+import Navbar from './Component/Navbar';
 import { Route, Routes } from 'react-router-dom';
-import Home from "./Pages/Home"
-import About from "./Pages/About"
-import Project from "./Pages/Project"
-import Contact from "./Pages/Contact"
-import Footer from './Conponent/Footer';
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Project from "./Pages/Project";
+import Contact from "./Pages/Contact";
+import Footer from './Component/Footer';
 
 const App = () => {
   return (
     <div>
-      <Navbar/>
+      <Navbar />
 
       <main>
         <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="about" element={<About/>} />
-          <Route path="project" element={<Project/>} />
-          <Route path="contact" element={<Contact/>} />
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="project" element={<Project />} />
+          <Route path="contact" element={<Contact />} />
         </Routes>
       </main>
 
-      <Footer/>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
-// import { StrictMode } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { HashRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App/>
-    </BrowserRouter>
-  </React.StrictMode>
+  <StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </StrictMode>,
 )
